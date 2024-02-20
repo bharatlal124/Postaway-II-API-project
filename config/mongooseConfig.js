c3dcc4ef1bcbb3896602639093258eaa6ebd55bc
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-02-20 12:42:39 +0530

    Add files via upload

---
+++ config/mongooseConfig.js
@@ -0,0 +1,19 @@
+// Creating connection with mongodb database here.
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const url = process.env.DB_URL;
+
+export const connectUsingMongoose = async()=>{
+    try {
+        await mongoose.connect(url, {
+            family: 4 
+        });
+        console.log("Mongodb connected using mongoose.");
+    } catch (error) {
+        console.log("Error while connecting to db");
+        console.log(error);
+    }
+}
